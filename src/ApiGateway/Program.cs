9c4002a2f9ab7f3cc168feeeb11f3f7ae0ebdbe0
Lazar Jovic <ljovic192@gmail.com>
2026-06-14 23:43:37 +0200

    Added ingress gateway

---
+++ src/ApiGateway/Program.cs
@@ -0,0 +1,53 @@
+using Yarp.ReverseProxy.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromMemory(
+        new[]
+        {
+            new RouteConfig
+            {
+                RouteId = "ingest",
+                ClusterId = "ingestion",
+                Match = new RouteMatch { Path = "/api/ingest/{**catch-all}" }
+            },
+            new RouteConfig
+            {
+                RouteId = "sensors",
+                ClusterId = "ingestion",
+                Match = new RouteMatch { Path = "/api/sensors/{**catch-all}" }
+            },
+            new RouteConfig
+            {
+                RouteId = "alarms",
+                ClusterId = "notification",
+                Match = new RouteMatch { Path = "/api/alarms/{**catch-all}" }
+            }
+        },
+        new[]
+        {
+            new ClusterConfig
+            {
+                ClusterId = "ingestion",
+                Destinations = new Dictionary<string, DestinationConfig>
+                {
+                    { "d1", new DestinationConfig { Address = "http://ingestion-service:8080/" } }
+                }
+            },
+            new ClusterConfig
+            {
+                ClusterId = "notification",
+                Destinations = new Dictionary<string, DestinationConfig>
+                {
+                    { "d1", new DestinationConfig { Address = "http://notification-service:8080/" } }
+                }
+            }
+        }
+    );
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.Run();
